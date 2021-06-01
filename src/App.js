ec3c595b8a88e0a5c659b2e30eed07e9bde3f9dd
Adam Jarrold <adam.jarrold@d3r.com>
2021-06-01 11:37:24 +0100

    Project set-up

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Pet = () => {
+  return React.createElement('div', {}, [
+    React.createElement('h2', {}, 'Luna'),
+    React.createElement('h3', {}, 'Dog'),
+    React.createElement('h3', {}, 'Havanese'),
+  ]);
+};
+
+const App = () => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, 'Adopt Me!'),
+    React.createElement(Pet),
+    React.createElement(Pet),
+    React.createElement(Pet),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
